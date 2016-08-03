affc45425aec5b1bcf37a993cc9d0bccbeb7a03c
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-08-03 18:28:30 +0200

    Add converting GenerateShipsEvent to EventConverter.

--- client/src/main/java/io/github/expansionteam/battleships/logic/EventConverter.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/EventConverter.java
@@ -1,5 +1,6 @@
 package io.github.expansionteam.battleships.logic;
 
+import io.github.expansionteam.battleships.logic.events.GenerateShipsEvent;
 import io.github.expansionteam.battleships.logic.events.StartGameEvent;
 import org.json.JSONObject;
 
@@ -7,6 +8,10 @@ public class EventConverter {
 
     public String convertToJson(StartGameEvent event) {
         return new JSONObject().put("type", "StartGameEvent").toString();
+    }
+
+    public String convertToJson(GenerateShipsEvent event) {
+        return new JSONObject().put("type", "GenerateShipsEvent").toString();
     }
 
 }
