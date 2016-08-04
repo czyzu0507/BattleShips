cb79c3854de17584f7f8b99310a0ee2413761bb2
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-08-04 17:25:41 +0200

    Implement MessageSender.

---
+++ client/src/test/java/io/github/expansionteam/battleships/logic/message/MessageSenderTest.java
@@ -0,0 +1,12 @@
+package io.github.expansionteam.battleships.logic.message;
+
+import org.testng.annotations.Test;
+
+public class MessageSenderTest {
+
+    @Test
+    public void sendMessageAndWaitForResponse() {
+
+    }
+
+}
