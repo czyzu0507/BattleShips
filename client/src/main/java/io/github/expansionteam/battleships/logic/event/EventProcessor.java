e7d9fde96d8a97533f242998fecba5e742c55658
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-08-04 17:32:20 +0200

    Add logging.

--- client/src/main/java/io/github/expansionteam/battleships/logic/event/EventProcessor.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/event/EventProcessor.java
@@ -8,8 +8,11 @@ import io.github.expansionteam.battleships.logic.message.Message;
 import io.github.expansionteam.battleships.logic.message.MessageFactory;
 import io.github.expansionteam.battleships.logic.message.MessageProcessor;
 import io.github.expansionteam.battleships.logic.message.ProcessMessageTask;
+import org.apache.log4j.Logger;
 
 public class EventProcessor {
+
+    private final static Logger log = Logger.getLogger(EventProcessor.class);
 
     private final AsyncTask asyncTask;
     private final MessageFactory messageFactory;
@@ -23,11 +26,15 @@ public class EventProcessor {
     }
 
     public void processEvent(StartGameEvent event) {
+        log.debug("Process StartGameEvent.");
+
         Message message = messageFactory.createFromEvent(event);
         asyncTask.runLater(new ProcessMessageTask(messageProcessor, message));
     }
 
     public void processEvent(GenerateShipsEvent event) {
+        log.debug("Process GenerateShipsEvent.");
+
         Message message = messageFactory.createFromEvent(event);
         asyncTask.runLater(new ProcessMessageTask(messageProcessor, message));
     }
