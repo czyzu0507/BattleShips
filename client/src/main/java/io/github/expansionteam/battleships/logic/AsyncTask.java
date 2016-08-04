b63bb08bd6662acbbdf91cc427c6af6cc52a80f9
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-08-04 14:19:00 +0200

    AsyncTask is only simple wrapper for static method from Platform object. It will allow us to test easier.

--- client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
@@ -1,8 +1,11 @@
 package io.github.expansionteam.battleships.logic;
 
+import javafx.application.Platform;
+
 public class AsyncTask {
 
     public void runLater(Runnable runnable) {
+        Platform.runLater(runnable);
     }
 
 }
