466f570f1619888c58f323dab26e0d6adda5d16f
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-08-04 15:47:35 +0200

    Implement message class that is simple value object.

--- client/src/main/java/io/github/expansionteam/battleships/logic/message/Message.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/message/Message.java
@@ -1,4 +1,15 @@
 package io.github.expansionteam.battleships.logic.message;
 
 public class Message {
+
+    private final String body;
+
+    public Message(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
 }
