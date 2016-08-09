b5e26abfb3813bcdf5c8c0da9b3cd9442bda8207
Kamil <kamilpoprawa@vp.pl>
2016-08-09 15:05:39 +0200

    AsyncTask simplified. Hangs of the GUI are avoided now. Now, new Thread is created from Runnable (instead of creating Task and other FX features).

--- client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
@@ -1,19 +1,9 @@
 package io.github.expansionteam.battleships.logic;
 
-import javafx.application.Platform;
-import javafx.concurrent.Task;
-
 public class AsyncTask {
 
     public void runLater(Runnable runnable) {
-        Task<Void> task = new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-                Platform.runLater(runnable);
-                return null;
-            }
-        };
-
-        new Thread(task).start();
+        new Thread(runnable).start();
     }
+    
 }
