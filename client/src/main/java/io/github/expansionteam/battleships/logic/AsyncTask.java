a3b880aaf75e4d02a7e2de379804d87962cb27a9
Tomasz Dudzik <tomaszdudzik.web@gmail.com>
2016-08-11 11:36:57 +0200

    Use ExecutorService instead of creating new Thread manually.

--- client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
+++ client/src/main/java/io/github/expansionteam/battleships/logic/AsyncTask.java
@@ -1,9 +1,17 @@
 package io.github.expansionteam.battleships.logic;
 
+import java.util.concurrent.ExecutorService;
+
 public class AsyncTask {
 
-    public void runLater(Runnable runnable) {
-        new Thread(runnable).start();
+    private final ExecutorService executorService;
+
+    public AsyncTask(ExecutorService executorService) {
+        this.executorService = executorService;
     }
-    
+
+    public void runLater(Runnable runnable) {
+        executorService.execute(runnable);
+    }
+
 }
